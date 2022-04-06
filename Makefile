f944d5b9594752214802fbee5214a62c75707d61
SAFACAN KOK <SAFACAN001@e.ntu.edu.sg>
2022-04-07 00:47:04 +0800

    update

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	sudo python3 setup.py install
+
+clean:
+	sudo rm -rf build dist Rubiks2x2x2-OptimalSolver.egg-info
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as readme_file:
 setup(
     name='kociemba',
     version='1.2.1',
-    description='Python/C implementation of Herbert Kociemba\'s 2x2 optimal solver',
+    description='Python implementation of Herbert Kociemba\'s 2x2 optimal solver',
     long_description=readme,
     long_description_content_type='text/markdown',
     keywords='kociemba 2x2 rubik cube solver',
@@ -17,7 +17,7 @@ setup(
     author='safacan',
     author_email='kokcansafa7@gmail.com',
     license='GPLv2',
-    packages=['kociemba_2x2'],
+    packages=["kociemba_2x2"],
     classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
